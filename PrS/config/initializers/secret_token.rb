dac18aafbf4c4e275f0d2eb3fb87f53e956a69a4
TonGarcia <ton_unb@hotmail.com>
2013-10-22 09:09:24 -0200

    [Ilton Garcia]: Commit com a base do projeto Gateway (PHP) e do PrS (Rails).

---
+++ PrS/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SisPrs::Application.config.secret_key_base = '4bd0120489ccd46319b378fae5a6cc8adcdacee3313797b2b954fc0f3953e5494d5ca651535cdf1eac6467284cf37c4daa29da56512b8565281aec1255aea14b'
---
+++ PrS/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SisPrs::Application.config.session_store :cookie_store, key: '_Sis-PrS_session'
